649182aaf7f5538e9478b2ba82235036b28f6621
MARIFE <MARIFE@DESKTOP-DO19G7T>
2022-01-20 12:07:43 -0500

    Tipo Movimiento CRUD

--- rrhh-web/src/main/java/ec/mil/ejercito/finanzas/rrhh/beans/TipoMovimientoBean.java
+++ rrhh-web/src/main/java/ec/mil/ejercito/finanzas/rrhh/beans/TipoMovimientoBean.java
@@ -12,6 +12,8 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
+
 import ec.mil.ejercito.finanzas.rrhh.controllers.TipoMovimientoController;
 import ec.mil.ejercito.finanzas.rrhh.entities.FtmoTipmovimiento;
 import ec.mil.ejercito.finanzas.rrhh.util.FacesUtil;
@@ -42,6 +44,9 @@ public class TipoMovimientoBean extends FacesUtil implements Serializable {
 	@Getter
 	@Setter
 	private List<FtmoTipmovimiento> listaTipoMovimiento;
+	@Getter
+	@Setter
+	private boolean esConciliable;
 
 	@Inject
 	private TipoMovimientoController adminTipoMovimiento;
@@ -59,6 +64,64 @@ public class TipoMovimientoBean extends FacesUtil implements Serializable {
 			this.listaTipoMovimiento = adminTipoMovimiento.consultarTodos();
 		} catch (Exception e) {
 			anadirError("Problema al cargar tipos de movimiento:" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Método para guardar el tipo de movimiento
+	 */
+	public void guardar() {
+		try {
+			String mensaje = null;
+			tipoMovimiento.setTmoConciliable(esConciliable == true ? "S" : "N");
+			if (tipoMovimiento.getTmoCodigo() > 0) {
+				mensaje = adminTipoMovimiento.actualizar(tipoMovimiento);
+			} else {
+				mensaje = adminTipoMovimiento.guardar(tipoMovimiento);
+			}
+			anadirInfo(mensaje);
+			cargarTiposMovimiento();
+			this.tipoMovimiento = new FtmoTipmovimiento();
+		} catch (Exception e) {
+			anadirError("Problema al guardar tipo de movimiento:" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Método para seleccionar tipo de movimiento
+	 * 
+	 * @param ev
+	 */
+	public void seleccionarRegistro(SelectEvent<FtmoTipmovimiento> ev) {
+		this.tipoMovimientoSel = ev.getObject();
+	}
+
+	/**
+	 * Método para editar un registro
+	 */
+	public void editar() {
+		if (tipoMovimientoSel != null) {
+			tipoMovimiento = tipoMovimientoSel;
+		} else {
+			anadirError("Se debe seleccionar un registro");
+		}
+	}
+
+	/**
+	 * Método para eliminar un tipo de movimiento
+	 */
+	public void eliminar() {
+		try {
+			if (tipoMovimientoSel != null) {
+				String mensaje = adminTipoMovimiento.eliminar(tipoMovimientoSel);
+				anadirInfo(mensaje);
+				cargarTiposMovimiento();
+			} else {
+				anadirError("Se debe seleccionar un registro");
+			}
+		} catch (Exception e) {
+			anadirError("Problema al eliminar un tipo de movimiento:" + e.getMessage());
+
 		}
 	}
 
